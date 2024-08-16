e9de56e94bf8949c0ebd6d1d270748ed1313d3c4
weakbox <connormcleod2000@icloud.com>
2024-08-16 16:58:32 -0300

    Button now evaluates how close a guess is.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import './globals.scss';
 import questions from './assets/questions.json';
+import Card from './components/Card';
 
 const yearMin = 1924;
 const yearMax = 2024;
 
 const getRandomQuestion = () => {
   return questions[Math.floor(Math.random() * questions.length)];
+};
+
+const evaluateGuess = (guess, year) => {
+  const difference = Math.abs(parseInt(year) - guess);
+  return `Your guess: ${guess}\nActual Year: ${year}\nYou were ${difference} years off.`;
 };
 
 function App() {
@@ -17,7 +23,9 @@ function App() {
     setYear(event.target.value);
   };
 
-  const changeQuestion = () => {
+  const handleGuess = () => {
+    const string = evaluateGuess(year, question.year);
+    alert(string);
     setQuestion(getRandomQuestion());
   };
 
@@ -26,12 +34,12 @@ function App() {
       <p className="text-center text-muted">In what year did the following happen?</p>
       <h2 id="question-text" className="text-center">{ question.text }</h2>
       <img id="question-img" src={ question.img }/>
-      <div className="year-container">
+      <div className="year-container text-center">
         <p className="text-bold text-center">Your Guess:</p>
         <input type="number" id="guess-year" value={ year } onChange={ changeYear } min={ yearMin } max={ yearMax }/>
       </div>
       <input id="guess-slider" type="range" value={ year } onChange={ changeYear } min={ yearMin } max={ yearMax }/>
-      <button id="guess-button" onClick={ changeQuestion }>Confirm</button>
+      <button id="guess-button" onClick={ handleGuess }>Confirm</button>
     </div>
   );
 }

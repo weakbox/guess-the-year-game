e6c10b4212f46f48815b44b2143b89c0d8c6a16a
weakbox <connormcleod2000@icloud.com>
2024-08-16 15:42:09 -0300

    Added slider functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,20 @@ import { useState } from 'react'
 import './globals.scss';
 import questions from './assets/questions.json';
 
+const yearMin = 1924;
+const yearMax = 2024;
+
 const getRandomQuestion = () => {
   return questions[Math.floor(Math.random() * questions.length)];
 };
 
 function App() {
   const [question, setQuestion] = useState(getRandomQuestion());
+  const [year, setYear] = useState(yearMax);
+
+  const changeYear = (event) => {
+    setYear(event.target.value);
+  };
 
   const changeQuestion = () => {
     setQuestion(getRandomQuestion());
@@ -20,9 +28,9 @@ function App() {
       <img id="question-img" src={ question.img }/>
       <div className="year-container">
         <p className="text-bold text-center">Your Guess:</p>
-        <input type="number" id="guess-year"></input>
+        <input type="number" id="guess-year" value={ year } onChange={ changeYear } min={ yearMin } max={ yearMax }/>
       </div>
-      <input id="guess-slider" type="range" min="1924" max="2024"></input>
+      <input id="guess-slider" type="range" value={ year } onChange={ changeYear } min={ yearMin } max={ yearMax }/>
       <button id="guess-button" onClick={ changeQuestion }>Confirm</button>
     </div>
   );

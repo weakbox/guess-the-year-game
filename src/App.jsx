d1c033062ddd5d2c6a13dea80843f0226b9947e0
weakbox <connormcleod2000@icloud.com>
2024-08-16 14:41:58 -0300

    Added button functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-import './globals.scss'
+import { useState } from 'react'
+import './globals.scss';
+import questions from './assets/questions.json';
+
+const getRandomQuestion = () => {
+  return questions[Math.floor(Math.random() * questions.length)];
+};
 
 function App() {
+  const [question, setQuestion] = useState(getRandomQuestion());
+
+  const changeQuestion = () => {
+    setQuestion(getRandomQuestion());
+  };
+
   return (
     <div className="app-container">
       <p className="text-center text-muted">In what year did the following happen?</p>
-      <h2 id="question-text" className="text-center">The CN Tower is opened for the first time.</h2>
-      <img id="question-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Toronto_-_ON_-_Toronto_Harbourfront7.jpg/375px-Toronto_-_ON_-_Toronto_Harbourfront7.jpg" alt="The CN Tower" />
+      <h2 id="question-text" className="text-center">{ question.text }</h2>
+      <img id="question-img" src={ question.img }/>
       <p className="text-bold">Guess</p>
       <p id="guess-year">2024</p>
       <input id="guess-slider" type="range" min="1924" max="2024"></input>
-      <button id="guess-button">Confirm</button>
+      <button id="guess-button" onClick={ changeQuestion }>Confirm</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

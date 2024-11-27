cb64ab4f65d5fb5c50b2c24526fb1dfbbcce268d
weakbox <connormcleod2000@icloud.com>
2024-11-27 11:15:28 -0400

    Added sound effects.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import './globals.scss';
 import questions from './assets/video_games.json';
 import XPCard from './components/XPCard';
 
+// Sound imports:
+import critical_stop from "/critical_stop.mp3";
+import exclamation from "/exclamation.mp3";
+import tada from "/tada.mp3";
+
 const yearMin = 1924;
 const yearMax = 2024;
 
@@ -12,15 +17,16 @@ const getRandomQuestion = () => {
 
 const evaluateGuess = (guess, year) => {
   const difference = Math.abs(parseInt(year) - guess);
-  // playSound(difference);
+  playSound(difference);
   return [`Your guess: ${guess}`, `Actual Year: ${year}`, `You were ${difference} years off.`];
 };
 
 const playSound = (difference) => {
   const sound = new Audio();
-  if (difference >= 10) sound.src = `https://www.101soundboards.com/sounds/14500-critical-stop`;
-  else if (difference >= 1) sound.src = `https://www.101soundboards.com/sounds/14504-exclamation`;
-  else sound.src = `https://www.101soundboards.com/sounds/14523-tada`;
+  sound.volume = 0.5;
+  if (difference >= 10) sound.src = critical_stop;
+  else if (difference >= 1) sound.src = exclamation;
+  else sound.src = tada;
   sound.play();
 };
 
@@ -79,7 +85,19 @@ function App() {
         <input id="guess-slider" type="range" value={ year } onChange={ changeYear } min={ yearMin } max={ yearMax }/>
         <button id="guess-button" onClick={ handleGuess }>Confirm Guess</button>
       </XPCard>
-      {showResult && <div className="result-container"><XPCard icon="./question_icon.png" title="Results!" bodyText={evaluateGuess(year, question.year).map(element => <p>{element}</p>)}><button onClick={handleGuess}>Go Back</button></XPCard></div>}
+      {showResult && (
+        <div className="result-container">
+          <XPCard
+            icon="./question_icon.png"
+            title="Results!"
+            bodyText={evaluateGuess(year, question.year).map((element, index) => (
+              <p key={index}>{element}</p>
+            ))}
+          >
+            <button onClick={handleGuess}>Go Back</button>
+          </XPCard>
+        </div>
+      )}
     </div>
   );
 }
